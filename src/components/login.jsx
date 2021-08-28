7360c5fb3e3f93812bbbbe872a8b29da67088e6f
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-29 02:14:36 +0530

    completed styling of login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-function Login() {
+function Login(props) {
   return (
     <Container>
       <Nav>
@@ -10,8 +10,21 @@ function Login() {
         </a>
         <div>
           <Join>Join Now</Join>
+          <SignIn>Sign In</SignIn>
         </div>
       </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your Proffesional community.</h1>
+          <img src="/images/login-hero.svg"></img>
+        </Hero>
+        <Form>
+          <Google>
+            <img src="/images/google.svg" />
+            Sign in with Google
+          </Google>
+        </Form>
+      </Section>
     </Container>
   );
 }
@@ -20,7 +33,7 @@ const Container = styled.div`
   padding: 0;
 `;
 const Nav = styled.nav`
-  max-width: 1128px;
+  max-width: 1200px;
   margin: auto;
   padding: 12px 0 16px;
   display: flex;
@@ -34,6 +47,7 @@ const Nav = styled.nav`
     height: 34px;
     @media (max-width: 768px) {
       padding: 0 5px;
+      margin: 0 10px;
     }
   }
 `;
@@ -42,6 +56,106 @@ const Join = styled.a`
   padding: 10px 12px;
   text-decoration: none;
   color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  transition: all ease 0.3s;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 1.6);
+  }
+`;
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 25px;
+  transition-duration: 167ms;
+  padding: 10px 25px;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 40px;
+  text-align: center;
+  transition: all ease-in 0.2s;
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+  }
+`;
+const Section = styled.section`
+  display: flex;
+  align-items: start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding-inline: 60px;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1200px;
+  align-items: center;
+  margin: auto;
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  } ;
+`;
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+  img {
+    /* z-index: -1; */
+    height: 670px;
+    width: 700px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
+    }
+  }
+`;
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  } ;
+`;
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: white;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 25px;
+  box-shadow: inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 /0);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.85);
+  }
 `;
 
 export default Login;
