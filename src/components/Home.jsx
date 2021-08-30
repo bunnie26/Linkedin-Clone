0bf92c612f0402844b460ead89d590110cf11982
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-31 00:00:31 +0530

    random changes n main page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,12 +7,17 @@ function Home(props) {
       <Section>
         <h5>
           <a>Hiring in a hurry? - </a>
-          <p>
-            Find talented pros in record time with Upwork and keep business
-            moving.
-          </p>
         </h5>
+        <p>
+          Find talented pros in record time with Upwork and keep business
+          moving.
+        </p>
       </Section>
+      <Layout>
+        <div>Left Side</div>
+        <div>Main</div>
+        <div>Right Side</div>
+      </Layout>
     </Container>
   );
 }
@@ -33,8 +38,39 @@ const Section = styled.section`
   text-align: center;
   text-decoration: underline;
   display: flex;
-  flex-direction: row;
   justify-content: center;
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+  p {
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  grid-template-rows: auto;
+  margin: 25px 0;
+  padding: 0 5px;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
 `;
 
 export default Home;
