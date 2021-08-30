7787c22f3038e8eb3b50cab25d6624cd1680686e
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-30 21:37:56 +0530

    started making home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 function Home(props) {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? - </a>
+          <p>
+            Find talented pros in record time with Upwork and keep business
+            moving.
+          </p>
+        </h5>
+      </Section>
+    </Container>
+  );
 }
 
-export default Home
+const Container = styled.div`
+  padding-top: 53px;
+  max-width: 100%;
+`;
+const Content = styled.div`
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+const Section = styled.section`
+  min-height: 50px;
+  padding: 16px 0;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export default Home;
